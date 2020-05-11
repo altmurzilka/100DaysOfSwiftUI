6f04c5a296557a2c2f43110f10c07954b5636dba
altmurzilka <altmurzilka@gmail.com>
2020-05-12 00:53:43 +0600

    all done!

--- RPS/RPS/ContentView.swift
+++ RPS/RPS/ContentView.swift
@@ -28,18 +28,20 @@ struct ContentView: View {
                 HStack {
                     Text("Score: \(score)")
                     Spacer()
-                    Text("Round \(round)/10)")
+                    Text("Round: \(round)/10")
                 }.padding(50)
                 Text("Now you need to: ")
                 Text(winOrLoose ? "WIN" : "LOSE")
                 Text("Against:")
                 Text(moves[randomMove])
-                    .font(.system(size: 80))
-                HStack(spacing: 10.0) {
+                    .font(.system(size: 60))
+                Spacer()
+                HStack(spacing: 20.0) {
                     ForEach(0 ..< moves.count) { number in
                         Button(action: {
                             self.buttonTapped(number)
                             self.round += 1
+                            self.nextMove()
                         }) {
                             Text(self.moves[number])
                                 .font(.system(size: 80))
@@ -55,6 +57,11 @@ struct ContentView: View {
         }
     }
     
+   func nextMove() {
+        randomMove = Int.random(in: 0 ... 2)
+        winOrLoose = Bool.random()
+    }
+    
     func restartGame() {
         round = 1
         randomMove = Int.random(in: 0 ... 2)
@@ -62,42 +69,24 @@ struct ContentView: View {
         score = 0
     }
     
-    
-    func selectMove(at index: Int) {
+    func buttonTapped(_ index: Int) {
         showingAlert = round >= 10
-        let userSelection = moves[index]
-        let gameSelection = moves[randomMove]
-        switch (userSelection.wins(against: gameSelection), winOrLoose) {
-        case (true, true), (false, false):
+        if (randomMove == 0 && index == 1 && winOrLoose == true) || (randomMove == 1 && index == 2 && winOrLoose == true) || (randomMove == 2 && index == 0 && winOrLoose == true)
+        {
             score += 1
-        default:
+        }
+        else if (randomMove == 0 && index == 2 && winOrLoose == false) || (randomMove == 1 && index == 0 && winOrLoose == false) || (randomMove == 2 && index == 1 && winOrLoose == false)
+        {
+            score += 1
+        }
+        else
+        {
             score -= 1
         }
         
-        randomMove = Int.random(in: 0 ... 2)
-        winOrLoose = Bool.random()
-        round += 1
-    }
-    
-    func buttonTapped(at index: Int) {
-        if randomMove == index {
-        } else if (randomMove == 0 && number == 1 && playerShouldWin == true) || (randomMove == 1 && number == 2 && playerShouldWin == true) || (randomMove == 2 && number == 0 && playerShouldWin == true) {
-            score += 1
-        } else if (randomMove == 0 && number == 2 && playerShouldWin == false) || (randomMove == 1 && number == 0 && playerShouldWin == false) || (randomMove == 2 && number == 1 && playerShouldWin == false) {
-            score += 1
-        } else {
-            score -= 1
+        if round == 10 {
+            showingAlert = true
         }
-        
-        if round > 10 {
-            print(round)
-            alertTitle = "Game Over! Your score is \(score)"
-            alertMessage = "Do you want to play it again?"
-            score = 0
-            round = 1
-        }
-        
-        showingAlert = true
     }
     
 }
