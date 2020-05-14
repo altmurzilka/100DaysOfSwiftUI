0d25e523ac2dacf74178d3cbab65db461b1bf9ca
altmurzilka <altmurzilka@gmail.com>
2020-05-15 03:04:57 +0600

    day 29

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by алтынпончик on 5/15/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
