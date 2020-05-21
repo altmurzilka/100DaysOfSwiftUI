4643631a57e5469c6dcde2ffd72b8c1cfebe80fd
altmurzilka <altmurzilka@gmail.com>
2020-05-22 02:57:44 +0600

    initial

---
+++ Edutainment/Edutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by алтынпончик on 5/22/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
