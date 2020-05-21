71648792f6efcbd5d2989cd36ae2ea07b6c8acf1
altmurzilka <altmurzilka@gmail.com>
2020-05-22 04:18:40 +0600

    skip day 35

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tipPercentage = 2
+    
+    @State private var number1 : Int = 0
+    @State private var number2 : Int = 0
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("How many qustions do you want")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationBarTitle("Edutainment", displayMode: .inline)
+            
+            .navigationBarItems(trailing: Button("Restart") {  // 2
+                self.startGame()
+            })
+        }
+    }
+    
+    func startGame() {
+        
     }
 }
 
