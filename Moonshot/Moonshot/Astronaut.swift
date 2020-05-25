b017655ffba19f43d2ffe3a453596d1504f5f237
altmurzilka <altmurzilka@gmail.com>
2020-05-26 05:52:57 +0600

    пошла мякота

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by алтынпончик on 5/26/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,9 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by алтынпончик on 5/26/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import Foundation
