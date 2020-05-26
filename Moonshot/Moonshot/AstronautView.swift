f738b9e787aad6b4f16f44931ffd88b4615da2f8
altmurzilka <altmurzilka@gmail.com>
2020-05-26 21:55:01 +0600

    day 41 part 2

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,21 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by алтынпончик on 5/26/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static var previews: some View {
+        AstronautView()
+    }
+}
