e2249cf50b07bd90942b16c3c0e7e1dbaae37160
altmurzilka <altmurzilka@gmail.com>
2020-06-03 15:35:03 +0600

    new project day 43

---
+++ Drawing/Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by алтынпончик on 6/3/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
