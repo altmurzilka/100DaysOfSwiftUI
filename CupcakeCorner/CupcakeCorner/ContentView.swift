6a217a1b9d050d9b7eeb744f6e4c43e33fa47145
altmurzilka <altmurzilka@gmail.com>
2020-06-24 04:30:56 +0600

    Day 49 project 10

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by алтынпончик on 6/24/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
