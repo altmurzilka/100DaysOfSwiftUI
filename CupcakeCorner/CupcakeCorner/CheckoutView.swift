8d69985bc5ffaf43d1222afd25282df5f1333b86
altmurzilka <altmurzilka@gmail.com>
2020-06-25 05:49:34 +0600

    Day 51 tutorials done!

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -9,7 +9,13 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    
     @ObservedObject var order: Order
+    // this related to the TextView
+    //   @State private var habitDescription = ""
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -18,18 +24,47 @@ struct CheckoutView: View {
                         .resizable()
                         .scaledToFit()
                         .frame(width: geo.size.width)
-
+                    
                     Text("Your total is $\(self.order.cost, specifier: "%.2f")")
                         .font(.title)
-
+                    // great stuff need to implement somewhere
+                    //   TextView(placeholderText: "Description", text: self.$habitDescription).frame(numLines: 15)
+                    
                     Button("Place Order") {
-                        // place the order
+                        self.placeOrder()
                     }
                     .padding()
                 }
             }
         }
         .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+        }.resume()
     }
 }
 
