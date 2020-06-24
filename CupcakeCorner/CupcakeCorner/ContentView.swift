a305c6f939913fd90b47b4a10180af363ba6c195
altmurzilka <altmurzilka@gmail.com>
2020-06-25 03:33:59 +0600

    day 50 done

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,16 +8,7 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
+// day 49 2nd video
 //class User: ObservableObject, Codable {
 //    enum CodingKeys: CodingKey {
 //        case name
@@ -37,42 +28,45 @@ struct Result: Codable {
 //}
 
 struct ContentView: View {
-    @State private var results = [Result]()
-
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .onAppear(perform: loadData)
-    }
+    @ObservedObject var order = Order()
     
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
                     }
-
-                    // everything is good, so we can exit
-                    return
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
+                    }
                 }
             }
-
-            // if we're still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
