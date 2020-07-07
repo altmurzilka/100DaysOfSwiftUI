bcd369b6a5ad8fe855a2b767ac7d7e633057e5b0
altyn <altmurzilka@gmail.com>
2020-07-08 03:07:34 +0600

    challenge day 59

---
+++ CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Алтын on 7/8/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
---
+++ CoreDataProject/Singer+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Singer+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Алтын on 7/8/20.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Singer {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Singer> {
+        return NSFetchRequest<Singer>(entityName: "Singer")
+    }
+
+    @NSManaged public var firstName: String?
+    @NSManaged public var lastName: String?
+    
+    var wrappedFirstName: String {
+        firstName ?? "Unknown"
+    }
+
+    var wrappedLastName: String {
+        lastName ?? "Unknown"
+    }
+}
