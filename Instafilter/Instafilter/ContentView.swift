0db789924ac868fac556157f2a314422aa1bbd16
altyn <altmurzilka@gmail.com>
2020-07-16 23:07:10 +0600

    lazy commit

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,25 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingActionSheet = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
+    @State private var showingImagePicker = false
     
     var body: some View {
-        Text("Hello, World!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                self.showingActionSheet = true
-            }.actionSheet(isPresented: $showingActionSheet) {
-                ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
-                    .default(Text("Red")) { self.backgroundColor = .red },
-                    .default(Text("Green")) { self.backgroundColor = .green },
-                    .default(Text("Blue")) { self.backgroundColor = .blue },
-                    .cancel()
-                ])
+        VStack {
+            image?ç
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                self.showingImagePicker = true
             }
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
     }
 }
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Алтын on 7/16/20.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        <#code#>
+    }
+        
+}
